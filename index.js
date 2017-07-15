fac3d3b8b6ec9e18d9d9b8f5ddeb1d7dd10eebc6
Robert Jackson <rjackson@linkedin.com>
2017-07-15 09:21:57 -0400

    Migrate back to `vendor` tree customized for Ember version.

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ var VersionChecker = require('ember-cli-version-checker');
 module.exports = {
   name: 'ember-let',
 
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    var checker = new VersionChecker(this);
+    this._emberDep = checker.forEmber();
+  },
+
   setupPreprocessorRegistry: function(type, registry) {
     // Inline let is only supported in Ember 2.0 and up.
-    var checker = new VersionChecker(this);
-    if (checker.forEmber().lt('2.0.0')) {
+    if (this._emberDep.lt('2.0.0')) {
       return;
     }
 
@@ -22,9 +28,34 @@ module.exports = {
     });
   },
 
-  contentFor: function(type) {
-    if (type === 'app-boot') {
-      return "require('ember-let/register');";
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    this._ensureThisImport();
+
+    let version;
+    if (this._emberDep.lt('2.10.0')) {
+      version = 'ember-lt-2-10';
+    } else if (this._emberDep.lt('2.13.0')) {
+      version = 'ember-lt-2-13';
+    } else if (this._emberDep.lt('2.15.0-alpha.1')) {
+      version = 'ember-lt-2-15';
+    } else if (this._emberDep.gte('2.15.0-alpha.1')) {
+      version = 'current';
     }
+
+    app.import(`vendor/ember-let/${version}/register.js`);
+  },
+
+  treeForVendor: function(rawVendorTree) {
+    let babelAddon = this.addons.find(addon => addon.name === 'ember-cli-babel');
+
+    let transpiledVendorTree = babelAddon.transpileTree(rawVendorTree, {
+      'ember-cli-babel': {
+        compileModules: false
+      }
+    });
+
+    return transpiledVendorTree;
   }
 };
---
+++ vendor/ember-let/ember-lt-2-10/register.js
@@ -0,0 +1,9 @@
+const { registerHelper } = Ember.__loader.require('ember-htmlbars/helpers');
+
+registerHelper('let', function letHelper(params, hash, options) {
+  let isBlock = !!options.template.yield;
+  // when used as a block
+    if (isBlock) {
+      options.template.yield(params);
+    }
+});
