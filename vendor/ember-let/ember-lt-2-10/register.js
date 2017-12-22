fadaf03236128d50983f15a1032e16cef8245b73
Eric Kelly <heroiceric@gmail.com>
2017-12-22 07:51:35 -0800

    HODL

--- vendor/ember-let/ember-lt-2-10/register.js
+++ vendor/ember-let/ember-lt-2-10/register.js
@@ -2,8 +2,9 @@ const { registerHelper } = Ember.__loader.require('ember-htmlbars/helpers');
 
 registerHelper('let', function letHelper(params, hash, options) {
   let isBlock = !!options.template.yield;
+
   // when used as a block
-    if (isBlock) {
-      options.template.yield(params);
-    }
+  if (isBlock) {
+    options.template.yield(params);
+  }
 });
